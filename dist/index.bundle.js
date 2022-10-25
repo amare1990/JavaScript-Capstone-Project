36d85c8264865b7a493a5f367124d5cb36667b78
amare1990 <amaremek@gmail.com>
2022-10-25 14:53:54 +0300

    Update the pachage.json to be tested

--- dist/index.bundle.js
+++ dist/index.bundle.js
@@ -115,7 +115,7 @@ eval("\n\n/* istanbul ignore next  */\nfunction styleTagTransform(css, styleElem
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _styles_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./styles.css */ \"./src/styles.css\");\n\n\nconst title = document.createElement('h1');\ntitle.className = 'title';\ntitle.innerHTML = 'Welcome!';\n\n//# sourceURL=webpack://javascript-capstone-project/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _styles_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./styles.css */ \"./src/styles.css\");\n\n\nconst title = document.createElement('h1');\ntitle.className = 'title';\ntitle.innerHTML = 'Welcome!';\nconst wrapper = document.querySelector('.wrapper');\nwrapper.append(title);\n\n//# sourceURL=webpack://javascript-capstone-project/./src/index.js?");
 
 /***/ })
 
