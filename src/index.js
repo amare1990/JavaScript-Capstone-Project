c7b68bb301796886ea1dcc8db89b59f96bbb19de
amare1990 <amaremek@gmail.com>
2022-10-25 12:54:44 +0300

    Finish Setup of webpack and Kanban

--- src/index.js
+++ src/index.js
@@ -1,27 +1,7 @@
 import './styles.css';
-import displayScore from './crud';
-import API_URL from './working';
-import postData from './display';
 
-const formAdd = document.querySelector('#form-add');
-const refresh = document.querySelector('#refresh-btn');
-const { name, score } = formAdd.elements;
-
-const addScore = (e) => {
-  e.preventDefault();
-  if (!name.value || !score.value) return;
-  const scoreData = {
-    user: name.value,
-    score: score.value,
-  };
-  postData(scoreData);
-};
-
-const renderAPI = async () => {
-  const fetchData = await fetch(API_URL);
-  const data = await fetchData.json();
-  displayScore(data.result);
-};
-
-formAdd.addEventListener('submit', addScore);
-refresh.addEventListener('click', renderAPI);
+const title = document.createElement('h1');
+title.className = 'title';
+title.innerHTML = 'Welcome!';
+const wrapper = document.querySelector('.wrapper');
+wrapper.append(title);
