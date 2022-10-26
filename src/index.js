aa38a796be38e5cdb5a7ac88a2bfbb6b36722bdc
amare1990 <amaremek@gmail.com>
2022-10-26 21:40:53 +0300

    Modify the footer section for the footer section

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import './styles.css';
-
-const title = document.createElement('h1');
-title.className = 'title';
-title.innerHTML = 'Welcome!';
-const wrapper = document.querySelector('.wrapper');
-wrapper.append(title);
