043224a04bfc7e912b21e9eb68677dafe485352b
amare1990 <amaremek@gmail.com>
2022-10-27 12:01:18 +0300

    Modify the getNumbeOfLikes to respond text

--- src/modules/likes.js
+++ src/modules/likes.js
@@ -16,8 +16,8 @@ const clickLoveBtn = async (itemId) => {
 
 const getNumberOfLikes = async () => {
   const response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/WCijE8yJ7svuzQLIyTFd/likes');
-  const responseJSON = await response.json();
-  return responseJSON;
+  const responseText = await response.text();
+  return responseText;
 };
 
 export { clickLoveBtn, getNumberOfLikes };
