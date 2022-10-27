aabe1b00ede9baddc8562e8e7afbdc247c2233c2
Ezema Anthony Sunday <tonyunday@gmail.com>
2022-10-27 08:53:43 -0700

    Display Comments pop up

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import getMenu from './modules/getmenu.js';
 
 import logo from './assets/resources/logo.png';
 /* import { clickLoveBtn, getNumberOfLikes } from './modules/likes.js'; */
-
 const parser = new DOMParser();
-
 const logoImage = new Image();
 logoImage.src = logo;
 logoImage.className = 'logo-img';
@@ -25,19 +23,27 @@ window.onload = async () => {
         <img src="${menuArray[i].strMealThumb}" alt="Meal Image" class="meal-img">
           <div class="meal-description">
             <p class="title">${menuArray[i].strMeal}</p>
-            <div class="like" id="${menuArray[i].idMeal}">        
+            <div class="like" id="${menuArray[i].idMeal}">
               <button type="button" class="click-like-btn">
               <i class="fa fa-heart like-btn"></i>
               </button>
             </div>
           </div>
-          <div class="likes-number"> 
+          <div class="likes-number">
             <span class="number-span"> </span>
             <p class="like-text"> likes </p>
           </div>
           <button type="button" class="comment-btn">Comments</button>
+          const commentbtn = stringElement.querySelector('.comment-btn');
+          commentbtn.addEventListener('click', (e) => {
+            e.preventDefault();
+            popUpSection.classList.remove('hidden');
+            showPopup(mealWithLike.idMeal);
+          });
+        });
       </div>`;
-    const parsedElement = parser.parseFromString(mealsGridsSring, 'text/html').body.firstChild;
+    const parsedElement = parser.parseFromString(mealsGridsSring, 'text/html')
+      .body.firstChild;
 
     menuGrids.append(parsedElement);
 
